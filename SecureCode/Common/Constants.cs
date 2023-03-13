b57d9bd2eefc63a52ec4e5ae0e430365624b64a9
rahulgo8u <rrsimt.rahul@gmail.com>
2023-03-13 18:00:57 +0400

    Implementing des encryption changes

---
+++ SecureCode/Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureCode.Common
+{
+    public static class Constants
+    {
+        //move this to configuration
+        public static string EncryptionKey = "63$#DHJngst";
+        public static byte[] IVArr = new byte[] { 89, 45, 34, 89, 21, 89, 54, 21 };
+    }
+}
---
+++ SecureCode/Security/DESEncryption.cs
@@ -0,0 +1,35 @@
+﻿using SecureCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureCode.Security
+{
+    public static class DESEncryption
+    {
+        #region Helper Functions
+        public static string EncryptText(string text)
+        {
+            string res = text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                string key = Constants.EncryptionKey;
+                byte[] keyArr = key.Length > 8 ? Encoding.UTF8.GetBytes(key.Substring(0, 8)) : Encoding.UTF8.GetBytes(key);
+                byte[] inputArr = new byte[text.Length];
+            }
+            return res;
+        }
+        public static string DecryptText(string text)
+        {
+            string res = text;
+            if (!string.IsNullOrEmpty(text))
+            {
+
+            }
+            return res;
+        }
+        #endregion Helper Functions
+    }
+}
