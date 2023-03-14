7291b508964cb628690fbf20d9e68388935de7d1
rahulgo8u <rrsimt.rahul@gmail.com>
2023-03-14 11:37:46 +0400

    Updating Read.Me for DES

---
+++ SecureCode/Security/AESEncryption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureCode.Security
+{
+    #region Helper Functions
+    public static string EncryptText(string text)
+    {
+        return DESEncrypt(text);
+    }
+    public static string DecryptText(string text)
+    {
+        return DESDecrypt(text);
+    }
+    #endregion Helper Functions
+    public static class AESEncryption
+    {
+    }
+}
